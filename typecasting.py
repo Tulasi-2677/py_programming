105734bba10995a8141d2a1d01ddfb5b774b2ecd
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-09 21:18:11 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+a="5"
+b="8"
+d=0
+c=8
+print(d+c)
+print(a+b)
+# explicit type casting
+print(int(a)+int(b))
+# explicit type casting
+a=8
+print(type(a))
+
+b=8.0
+print(type(b))
+
+
+c=a+b
+print(c)
+print(type(c))
