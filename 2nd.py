dda705b96ca6d1b99d9b1e2c60d13641b6563066
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-09 21:15:17 +0530

    Add files via upload

---
+++ 2nd.py
@@ -0,0 +1,11 @@
+print("tulasi",7,"hbhfdgy")
+print("Hii i\' am sila \n and \"i am \"a cs student")#'\n' for new line in a single string know as escape sequence
+"""
+multiple comment double tripe cout and singlr tripe 
+multipe value
+print("tulasi",7,"hbhfdgy") 
+print("tulasi",7,"hbhfdgy")
+print("tulasi",7,"hbhfdgy")
+print("tulasi",7,"hbhfdgy")"""
+print("hii",'o',9,8,'p',sep='-',end="009\n")
+print('rudra')#single value
