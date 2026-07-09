ffe562985b9dc1c05953281466c89df9e1a81ba0
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-09 21:25:25 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,25 @@
+
+# variable
+a = 8
+print(a)
+
+b = "rudra"
+print(b)
+
+c=4.7
+print(c)
+
+d=True
+print(d)
+
+e=None
+print(e)
+
+print(a+c)
+# print(a+b) error
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
