571dec1414841d4b9603fce304131e16fb5c8b51
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-10 21:32:19 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,26 @@
+name = "Tulasi"
+name1 = "rudra"
+name2 ="jhfyrieyfuh"
+name4= "rudra is"
+text= "java is older then python"
+#Python methodes
+name_slice = name[0:3]
+print(name_slice)
+print(name[1])
+print(name[1:4])
+print(name1[-4:-1])
+print(name1[1:4])
+print(name[:5])#same as [0:5]
+print(name[1:])#same as [1:5]
+print(name2[1:6:2])#1st [1:6] which is "jhfyri" , then skip 2 index from 'h' afeter that output will be 'hyi'
+print(len(name))#len() to find lenght of string
+print(len(name1))
+print(len(name2))
+print(name.endswith("si"))
+print(name.endswith("siaa"))
+print(name.startswith("tu"))
+print(name.startswith("Tul"))
+print(name1.capitalize())
+print(name4.capitalize())#only first letter is capitalize
+print(text.find("java"))
+print(text.replace("java", "pythone"))
