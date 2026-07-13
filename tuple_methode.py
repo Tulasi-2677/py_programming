5a24a0010c6f4315a165d5a0a0d9d36380571c7d
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-13 21:35:11 +0530

    Add files via upload

---
+++ tuple_methode.py
@@ -0,0 +1,15 @@
+a = (1,3,5,"apple",False,True,"rohan",8.9)#tuple is Immutabele
+print(type(a)) 
+print(a)
+print(a.count("apple"))
+
+fruits = ("apple", "banana", "orange")
+
+print(fruits.index("banana"))
+print("banana" in fruits)
+
+#my_tuple = (1, 2, 3)
+#my_tuple[0] = 10
+#print(my_tuple)TypeError: 'tuple' object does not support item assignment
+
+
