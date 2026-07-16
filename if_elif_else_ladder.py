62c674f929cebaad1219b8a1a6b0c519765c65fb
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-16 17:39:17 +0530

    Add files via upload

---
+++ if_elif_else_ladder.py
@@ -0,0 +1,10 @@
+a = int(input("enter your age :"))
+
+if(a>18):
+    print("eligible for vote")
+elif(a<0):
+    print("you are giving invalid age")
+
+else:
+    print("your not eligible for vote")
+    
