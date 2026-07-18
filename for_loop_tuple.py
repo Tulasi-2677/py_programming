c9c58cdf22a7883072d9dd0ede6029d368fcec78
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-18 22:22:58 +0530

    Add files via upload

---
+++ for_loop_tuple.py
@@ -0,0 +1,14 @@
+#for loop with tuple
+
+my_tuple = ("tulasi",21,"blue","star")
+
+for i in my_tuple:  #here the output will be elemnts in the tuple
+    print(i)
+
+print()
+
+#for loop with string
+f = "rudra"
+
+for i in f:
+    print(i)
