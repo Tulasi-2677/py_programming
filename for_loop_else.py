e1dc0cb48da3d0fef8e01ad1c6bf55812253f68b
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-18 22:40:24 +0530

    Clarify else block usage in for loop
    Added comment to clarify the purpose of the else block after the for loop.

--- for_loop_else.py
+++ for_loop_else.py
@@ -5,4 +5,4 @@ my_tuple = ("tulasi",21,"blue","star")
 for i in my_tuple:  #here the output will be elemnts in the tuple
     print(i)
 else:
-    print("Hii")    
+    print("Hii")    #after tuple elemnts are printed , then else part will be executed
