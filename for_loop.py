d28463983c35aacda5d660ccad5368a855f835c2
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-18 21:49:52 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,7 @@
+for i in range(6):#range(6) means it will print from 0 to number 5
+    print(i)  #output will be 0 1 2 3 4 5
+
+print()
+
+for x in range(0,20,6):  #(start,stop,step_size)
+    print(x,)    #output will be 0 6 12 18
