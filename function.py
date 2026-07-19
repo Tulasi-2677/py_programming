7d2ff4cbfee2fd409b8f031995c274a1a0001e9d
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-19 23:30:53 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,12 @@
+#In Python, functions are reusable blocks of code that perform a specific task
+
+
+def add(): #function defination
+    a = int(input("enter value for a:"))
+    b= int(input("enter value for b:"))
+    addition = a+b
+    print(addition)
+
+    
+
+add()    #function call
