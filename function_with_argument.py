e0b0a991a9ac6b625f2fb06e33975c98e44d166c
Tulasi-2677 <tulasi04dakua@gmail.com>
2026-07-21 22:52:25 +0530

    Add files via upload

---
+++ function_with_argument.py
@@ -0,0 +1,9 @@
+# Parameters are variables in the function definition.
+
+# Arguments are the actual values passed when calling the function.
+
+
+def add(a,b): # Here a,b are parameters
+    print("Sum = ",a+b)
+
+add(10, 20)  # 10 and 20 are arguments  
